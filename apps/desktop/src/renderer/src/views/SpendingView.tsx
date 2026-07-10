19a8f517f5c0f86bdcef809b8de1471962619bdb
Mattéo <matteo.lorenzoq@gmail.com>
2026-07-10 11:08:35 +0200

    feat(spending): compose Origin spending overview in SpendingView

--- apps/desktop/src/renderer/src/views/SpendingView.tsx
+++ apps/desktop/src/renderer/src/views/SpendingView.tsx
@@ -1,14 +1,28 @@
-import { CategoriesPanel } from '../components/categories/CategoriesPanel'
-import { RulesPanel } from '../components/rules/RulesPanel'
-import { TransactionsPanel } from '../components/transactions/TransactionsPanel'
+import { BudgetPromoCard } from '../components/spending-overview/BudgetPromoCard'
+import { CategoryBreakdownCard } from '../components/spending-overview/CategoryBreakdownCard'
+import { LatestTransactionsCard } from '../components/spending-overview/LatestTransactionsCard'
+import { SpendTrendCard } from '../components/spending-overview/SpendTrendCard'
+import { SpendingTabs } from '../components/spending-overview/SpendingTabs'
+import { UpcomingTransactionsCard } from '../components/spending-overview/UpcomingTransactionsCard'
 
-/** Dépenses : transactions, catégories et règles de catégorisation. */
+/** Spending : reproduction de l'onglet Overview du dashboard Origin (données mockées). */
 export function SpendingView(): React.JSX.Element {
   return (
     <div className="flex flex-col gap-6 p-8">
-      <TransactionsPanel />
-      <CategoriesPanel />
-      <RulesPanel />
+      <SpendingTabs />
+      <div className="grid grid-cols-[1fr_360px] items-start gap-6">
+        <div className="flex flex-col gap-6">
+          <SpendTrendCard />
+          <div className="grid grid-cols-2 gap-6">
+            <LatestTransactionsCard />
+            <UpcomingTransactionsCard />
+          </div>
+        </div>
+        <div className="flex flex-col gap-6">
+          <BudgetPromoCard />
+          <CategoryBreakdownCard />
+        </div>
+      </div>
     </div>
   )
 }
